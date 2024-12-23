18344f7800c5d7708b9ffcf05c7f88ca8bc91537
lLucy3 <rayfebriantomasila@students.amikom.ac.id>
2024-12-23 11:34:05 +0700

    update langganan

--- landing/application/views/transaksi_tampil.php
+++ landing/application/views/transaksi_tampil.php
@@ -1,5 +1,5 @@
 <div class="container container-custom mt-4">
-<h2>UIny blum ada, ntar dulu</h2>
+<h2>UI'nya blum ada wkwk</h2>
     <button id="pay-button" class="btn btn--green mt-3">Bayar</button>
 
     <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-BT-fGv3Nb3Tvb3hG"></script>
